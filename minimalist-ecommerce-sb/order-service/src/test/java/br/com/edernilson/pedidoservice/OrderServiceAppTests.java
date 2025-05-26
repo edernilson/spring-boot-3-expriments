9e439d57092269759591762228ef3a3f17d7d507
Eder Nilson <eder.nilson@pathfind.com.br>
2025-05-26 11:26:53 -0300

    Initial projects product-service and order-service

---
+++ minimalist-ecommerce-sb/order-service/src/test/java/br/com/edernilson/pedidoservice/OrderServiceAppTests.java
@@ -0,0 +1,13 @@
+package br.com.edernilson.pedidoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
